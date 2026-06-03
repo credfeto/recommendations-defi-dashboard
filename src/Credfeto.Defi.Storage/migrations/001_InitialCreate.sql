d2922a045bdca23d412f8e52a24dd9b4919926b7
Mark Ridgwell <credfeto@users.noreply.github.com>
2026-06-03 18:23:26 +0100

    fix: apply SQLFluff formatting to all SQL files
    - Re-format all stored procedures and tables to 2-space indentation
    - Remove aligned column spacing (single space before types)
    - Rename MERGE aliases Target/Source → Tgt/Src (TARGET and SOURCE are
      T-SQL reserved words flagged by RF04)
    
    Both tsqllint and sqlfluff now pass with zero errors.
    
    Prompt: pr still says there are errors

--- src/Credfeto.Defi.Storage/migrations/001_InitialCreate.sql
+++ src/Credfeto.Defi.Storage/migrations/001_InitialCreate.sql
@@ -2,10 +2,10 @@ IF OBJECT_ID(N'[dbo].[ApiCache]', N'U') IS NULL
   BEGIN
     CREATE TABLE [dbo].[ApiCache]
     (
-        [Key]       NVARCHAR(450)  NOT NULL,
-        [Data]      NVARCHAR(MAX)  NOT NULL,
-        [FetchedAt] DATETIMEOFFSET NOT NULL,
-        CONSTRAINT [PK_ApiCache] PRIMARY KEY ([Key])
+      [Key] NVARCHAR(450) NOT NULL,
+      [Data] NVARCHAR(MAX) NOT NULL,
+      [FetchedAt] DATETIMEOFFSET NOT NULL,
+      CONSTRAINT [PK_ApiCache] PRIMARY KEY ([Key])
     );
   END;
 GO
@@ -14,153 +14,172 @@ IF OBJECT_ID(N'[dbo].[ContractSecurity]', N'U') IS NULL
   BEGIN
     CREATE TABLE [dbo].[ContractSecurity]
     (
-        [Chain]                   NVARCHAR(100)  NOT NULL,
-        [Address]                 NVARCHAR(100)  NOT NULL,
-        [ParentAddress]           NVARCHAR(100)  NULL,
-        [IsOpenSource]            BIT            NULL,
-        [IsHoneypot]              BIT            NULL,
-        [IsProxy]                 BIT            NULL,
-        [BuyTax]                  FLOAT(53)      NULL,
-        [SellTax]                 FLOAT(53)      NULL,
-        [TransferTax]             FLOAT(53)      NULL,
-        [CannotBuy]               BIT            NULL,
-        [HoneypotWithSameCreator] BIT            NULL,
-        [TokenName]               NVARCHAR(MAX)  NULL,
-        [TokenSymbol]             NVARCHAR(MAX)  NULL,
-        [CheckedAt]               DATETIMEOFFSET NOT NULL,
-        CONSTRAINT [PK_ContractSecurity] PRIMARY KEY ([Chain], [Address])
+      [Chain] NVARCHAR(100) NOT NULL,
+      [Address] NVARCHAR(100) NOT NULL,
+      [ParentAddress] NVARCHAR(100) NULL,
+      [IsOpenSource] BIT NULL,
+      [IsHoneypot] BIT NULL,
+      [IsProxy] BIT NULL,
+      [BuyTax] FLOAT(53) NULL,
+      [SellTax] FLOAT(53) NULL,
+      [TransferTax] FLOAT(53) NULL,
+      [CannotBuy] BIT NULL,
+      [HoneypotWithSameCreator] BIT NULL,
+      [TokenName] NVARCHAR(MAX) NULL,
+      [TokenSymbol] NVARCHAR(MAX) NULL,
+      [CheckedAt] DATETIMEOFFSET NOT NULL,
+      CONSTRAINT [PK_ContractSecurity] PRIMARY KEY ([Chain], [Address])
     );
   END;
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[ApiCache_GetByKey]
-    @Key NVARCHAR(450)
+  @Key NVARCHAR(450)
 AS
 BEGIN
-    SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-    SELECT [Key],
-           [Data],
-           [FetchedAt]
-    FROM   [dbo].[ApiCache]
-    WHERE  [Key] = @Key;
+  SELECT
+    [Key],
+    [Data],
+    [FetchedAt]
+  FROM [dbo].[ApiCache]
+  WHERE [Key] = @Key;
 END;
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[ApiCache_Upsert]
-    @Key       NVARCHAR(450),
-    @Data      NVARCHAR(MAX),
-    @FetchedAt DATETIMEOFFSET
+  @Key NVARCHAR(450),
+  @Data NVARCHAR(MAX),
+  @FetchedAt DATETIMEOFFSET
 AS
 BEGIN
-    SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-    MERGE [dbo].[ApiCache] AS target
-    USING (SELECT @Key AS [Key]) AS source ON target.[Key] = source.[Key]
-    WHEN MATCHED THEN
-        UPDATE SET [Data]      = @Data,
-                   [FetchedAt] = @FetchedAt
-    WHEN NOT MATCHED THEN
-        INSERT ([Key], [Data], [FetchedAt])
-        VALUES (@Key, @Data, @FetchedAt);
+  MERGE [dbo].[ApiCache] AS Tgt
+  USING (SELECT @Key AS [Key]) AS Src ON Tgt.[Key] = Src.[Key]
+  WHEN MATCHED
+    THEN
+    UPDATE
+      SET
+        [Data] = @Data,
+        [FetchedAt] = @FetchedAt
+  WHEN NOT MATCHED
+    THEN
+    INSERT ([Key], [Data], [FetchedAt])
+    VALUES (@Key, @Data, @FetchedAt);
 END;
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[ContractSecurity_GetByChainAndAddress]
-    @Chain   NVARCHAR(100),
-    @Address NVARCHAR(100)
+  @Chain NVARCHAR(100),
+  @Address NVARCHAR(100)
 AS
 BEGIN
-    SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-    SELECT [Chain],
-           [Address],
-           [ParentAddress],
-           [IsOpenSource],
-           [IsHoneypot],
-           [IsProxy],
-           [BuyTax],
-           [SellTax],
-           [TransferTax],
-           [CannotBuy],
-           [HoneypotWithSameCreator],
-           [TokenName],
-           [TokenSymbol],
-           [CheckedAt]
-    FROM   [dbo].[ContractSecurity]
-    WHERE  [Chain]   = @Chain
-      AND  [Address] = @Address;
+  SELECT
+    [Chain],
+    [Address],
+    [ParentAddress],
+    [IsOpenSource],
+    [IsHoneypot],
+    [IsProxy],
+    [BuyTax],
+    [SellTax],
+    [TransferTax],
+    [CannotBuy],
+    [HoneypotWithSameCreator],
+    [TokenName],
+    [TokenSymbol],
+    [CheckedAt]
+  FROM [dbo].[ContractSecurity]
+  WHERE [Chain] = @Chain
+    AND [Address] = @Address;
 END;
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[ContractSecurity_GetChildrenByParentAddress]
-    @Chain         NVARCHAR(100),
-    @ParentAddress NVARCHAR(100)
+  @Chain NVARCHAR(100),
+  @ParentAddress NVARCHAR(100)
 AS
 BEGIN
-    SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-    SELECT [Chain],
-           [Address],
-           [ParentAddress],
-           [IsOpenSource],
-           [IsHoneypot],
-           [IsProxy],
-           [BuyTax],
-           [SellTax],
-           [TransferTax],
-           [CannotBuy],
-           [HoneypotWithSameCreator],
-           [TokenName],
-           [TokenSymbol],
-           [CheckedAt]
-    FROM   [dbo].[ContractSecurity]
-    WHERE  [Chain]         = @Chain
-      AND  [ParentAddress] = @ParentAddress;
+  SELECT
+    [Chain],
+    [Address],
+    [ParentAddress],
+    [IsOpenSource],
+    [IsHoneypot],
+    [IsProxy],
+    [BuyTax],
+    [SellTax],
+    [TransferTax],
+    [CannotBuy],
+    [HoneypotWithSameCreator],
+    [TokenName],
+    [TokenSymbol],
+    [CheckedAt]
+  FROM [dbo].[ContractSecurity]
+  WHERE [Chain] = @Chain
+    AND [ParentAddress] = @ParentAddress;
 END;
 GO
 
 CREATE OR ALTER PROCEDURE [dbo].[ContractSecurity_Upsert]
-    @Chain                   NVARCHAR(100),
-    @Address                 NVARCHAR(100),
-    @ParentAddress           NVARCHAR(100)  = NULL,
-    @IsOpenSource            BIT            = NULL,
-    @IsHoneypot              BIT            = NULL,
-    @IsProxy                 BIT            = NULL,
-    @BuyTax                  FLOAT(53)      = NULL,
-    @SellTax                 FLOAT(53)      = NULL,
-    @TransferTax             FLOAT(53)      = NULL,
-    @CannotBuy               BIT            = NULL,
-    @HoneypotWithSameCreator BIT            = NULL,
-    @TokenName               NVARCHAR(MAX)  = NULL,
-    @TokenSymbol             NVARCHAR(MAX)  = NULL,
-    @CheckedAt               DATETIMEOFFSET
+  @Chain NVARCHAR(100),
+  @Address NVARCHAR(100),
+  @ParentAddress NVARCHAR(100) = NULL,
+  @IsOpenSource BIT = NULL,
+  @IsHoneypot BIT = NULL,
+  @IsProxy BIT = NULL,
+  @BuyTax FLOAT(53) = NULL,
+  @SellTax FLOAT(53) = NULL,
+  @TransferTax FLOAT(53) = NULL,
+  @CannotBuy BIT = NULL,
+  @HoneypotWithSameCreator BIT = NULL,
+  @TokenName NVARCHAR(MAX) = NULL,
+  @TokenSymbol NVARCHAR(MAX) = NULL,
+  @CheckedAt DATETIMEOFFSET
 AS
 BEGIN
-    SET NOCOUNT ON;
+  SET NOCOUNT ON;
 
-    MERGE [dbo].[ContractSecurity] AS target
-    USING (SELECT @Chain AS [Chain], @Address AS [Address]) AS source
-        ON target.[Chain] = source.[Chain] AND target.[Address] = source.[Address]
-    WHEN MATCHED THEN
-        UPDATE SET [ParentAddress]           = @ParentAddress,
-                   [IsOpenSource]            = @IsOpenSource,
-                   [IsHoneypot]              = @IsHoneypot,
-                   [IsProxy]                 = @IsProxy,
-                   [BuyTax]                  = @BuyTax,
-                   [SellTax]                 = @SellTax,
-                   [TransferTax]             = @TransferTax,
-                   [CannotBuy]               = @CannotBuy,
-                   [HoneypotWithSameCreator] = @HoneypotWithSameCreator,
-                   [TokenName]               = @TokenName,
-                   [TokenSymbol]             = @TokenSymbol,
-                   [CheckedAt]               = @CheckedAt
-    WHEN NOT MATCHED THEN
-        INSERT ([Chain], [Address], [ParentAddress], [IsOpenSource], [IsHoneypot], [IsProxy],
-                [BuyTax], [SellTax], [TransferTax], [CannotBuy], [HoneypotWithSameCreator],
-                [TokenName], [TokenSymbol], [CheckedAt])
-        VALUES (@Chain, @Address, @ParentAddress, @IsOpenSource, @IsHoneypot, @IsProxy,
-                @BuyTax, @SellTax, @TransferTax, @CannotBuy, @HoneypotWithSameCreator,
-                @TokenName, @TokenSymbol, @CheckedAt);
+  MERGE [dbo].[ContractSecurity] AS Tgt
+  USING (
+    SELECT
+      @Chain   AS [Chain],
+      @Address AS [Address]
+  ) AS Src
+  ON Tgt.[Chain] = Src.[Chain] AND Tgt.[Address] = Src.[Address]
+  WHEN MATCHED
+    THEN
+    UPDATE
+      SET
+        [ParentAddress] = @ParentAddress,
+        [IsOpenSource] = @IsOpenSource,
+        [IsHoneypot] = @IsHoneypot,
+        [IsProxy] = @IsProxy,
+        [BuyTax] = @BuyTax,
+        [SellTax] = @SellTax,
+        [TransferTax] = @TransferTax,
+        [CannotBuy] = @CannotBuy,
+        [HoneypotWithSameCreator] = @HoneypotWithSameCreator,
+        [TokenName] = @TokenName,
+        [TokenSymbol] = @TokenSymbol,
+        [CheckedAt] = @CheckedAt
+  WHEN NOT MATCHED
+    THEN
+    INSERT (
+      [Chain], [Address], [ParentAddress], [IsOpenSource], [IsHoneypot], [IsProxy],
+      [BuyTax], [SellTax], [TransferTax], [CannotBuy], [HoneypotWithSameCreator],
+      [TokenName], [TokenSymbol], [CheckedAt]
+    )
+    VALUES (
+      @Chain, @Address, @ParentAddress, @IsOpenSource, @IsHoneypot, @IsProxy,
+      @BuyTax, @SellTax, @TransferTax, @CannotBuy, @HoneypotWithSameCreator,
+      @TokenName, @TokenSymbol, @CheckedAt
+    );
 END;
 GO
